80053720205d5b9efc582327978ac153f12d89e7
peter <mygemailpublic@gmail.com>
2015-12-30 20:11:01 +0000

    list_del additional cases

--- test/twtest.c
+++ test/twtest.c
@@ -40,8 +40,9 @@ twlist_test_list_head_init(void)
 static void
 twlist_test_list_add(void)
 {
-	struct twlist_head	h;
+	struct twlist_head	h, i;
 	struct gucio		g;
+	struct gucio		g1, g2, g3;
 
 	TWINIT_LIST_HEAD(&h);
 	twlist_add(&g.link, &h);
@@ -49,13 +50,39 @@ twlist_test_list_add(void)
 	assert(h.prev == &g.link);
 	assert(g.link.next == &h);
 	assert(g.link.prev == &h);
+	TWINIT_LIST_HEAD(&i);
+	g1.key = 1;
+	twlist_add(&g1.link, &i);
+	assert(i.next == &g1.link);
+	assert(i.prev == &g1.link);
+	assert(g1.link.next == &i);
+	assert(g1.link.prev == &i);
+	g2.key = 2;
+	twlist_add(&g2.link, &i);
+	assert(i.next == &g2.link);
+	assert(i.prev == &g1.link);
+	assert(g2.link.next == &g1.link);
+	assert(g2.link.prev == &i);
+	assert(g1.link.next == &i);
+	assert(g1.link.prev == &g2.link);
+	g3.key = 3;
+	twlist_add(&g3.link, &i);
+	assert(i.next == &g3.link);
+	assert(i.prev == &g1.link);
+	assert(g3.link.next == &g2.link);
+	assert(g3.link.prev == &i);
+	assert(g2.link.next == &g1.link);
+	assert(g2.link.prev == &g3.link);
+	assert(g1.link.next == &i);
+	assert(g1.link.prev == &g2.link);
 }
 
 static void
 twlist_test_list_add_tail(void)
 {
-	struct twlist_head	h;
+	struct twlist_head	h, i;
 	struct gucio		g;
+	struct gucio		g1, g2, g3;
 
 	TWINIT_LIST_HEAD(&h);
 	twlist_add_tail(&g.link, &h);
@@ -63,14 +90,40 @@ twlist_test_list_add_tail(void)
 	assert(h.prev == &g.link);
 	assert(g.link.next == &h);
 	assert(g.link.prev == &h);
+	TWINIT_LIST_HEAD(&i);
+	g1.key = 1;
+	twlist_add_tail(&g1.link, &i);
+	assert(i.next == &g1.link);
+	assert(i.prev == &g1.link);
+	assert(g1.link.next == &i);
+	assert(g1.link.prev == &i);
+	g2.key = 2;
+	twlist_add_tail(&g2.link, &i);
+	assert(i.next == &g1.link);
+	assert(i.prev == &g2.link);
+	assert(g2.link.next == &i);
+	assert(g2.link.prev == &g1.link);
+	assert(g1.link.next == &g2.link);
+	assert(g1.link.prev == &i);
+	g3.key = 3;
+	twlist_add_tail(&g3.link, &i);
+	assert(i.next == &g1.link);
+	assert(i.prev == &g3.link);
+	assert(g3.link.next == &i);
+	assert(g3.link.prev == &g2.link);
+	assert(g2.link.next == &g3.link);
+	assert(g2.link.prev == &g1.link);
+	assert(g1.link.next == &g2.link);
+	assert(g1.link.prev == &i);
 }
 
 
 static void
 twlist_test_list_del(void)
 {
-	struct twlist_head	h, i;
+	struct twlist_head	h, i, j;
 	struct gucio		g;
+	struct gucio		g1, g2, g3;
 
 	TWINIT_LIST_HEAD(&h);
 	twlist_del(&h);
@@ -87,6 +140,39 @@ twlist_test_list_del(void)
 	assert(i.prev == &i);
 	assert(g.link.next == TWLIST_POISON1);
 	assert(g.link.prev == TWLIST_POISON2);
+	TWINIT_LIST_HEAD(&j);
+	g1.key = 1;
+	twlist_add_tail(&g1.link, &j);
+	g2.key = 2;
+	twlist_add_tail(&g2.link, &j);
+	g3.key = 3;
+	twlist_add_tail(&g3.link, &j);
+	assert(g1.link.prev == &j);
+	assert(j.next == &g1.link);
+	assert(g1.link.next == &g2.link);
+	assert(g2.link.prev == &g1.link);
+	assert(g2.link.next == &g3.link);
+	assert(g3.link.prev == &g2.link);
+	assert(g3.link.next == &j);
+	twlist_del(&g2.link);
+	assert(g1.link.prev == &j);
+	assert(j.next == &g1.link);
+	assert(g1.link.next == &g3.link);
+	assert(g2.link.prev == TWLIST_POISON2);
+	assert(g2.link.next == TWLIST_POISON1);
+	assert(g3.link.prev == &g1.link);
+	assert(g3.link.next == &j);
+	twlist_del(&g1.link);
+	assert(g3.link.prev == &j);
+	assert(j.next == &g3.link);
+	assert(g1.link.prev == TWLIST_POISON2);
+	assert(g1.link.next == TWLIST_POISON1);
+	assert(g3.link.next == &j);
+	twlist_del(&g3.link);
+	assert(j.prev == &j);
+	assert(j.next == &j);
+	assert(g3.link.prev == TWLIST_POISON2);
+	assert(g3.link.next == TWLIST_POISON1);
 }
 
 static void
@@ -103,6 +189,7 @@ twlist_test_list_del_init(void)
 	assert(g.link.next == g.link.prev);
 	assert(g.link.prev == &g.link);
 }
+
 static void
 twlist_test(void)
 {
